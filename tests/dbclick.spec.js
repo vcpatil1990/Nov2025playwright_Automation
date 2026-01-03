2da6395ce4159aca178572aeab0c1044f6e71fdd
vapatil990 <vcp199013@gmail.com>
2026-01-03 21:00:39 +0530

    Changes in dbclick file

--- tests/dbclick.spec.js
+++ tests/dbclick.spec.js
@@ -3,14 +3,14 @@ const { test, expect } = require('@playwright/test');
 test('Double click on button', async ({ page }) => {
 
 
-    await page.goto("https://testautomationpractice.blogspot.com/");
-    await page.waitForTimeout(4000);
-    let copytext = await page.locator("//button[text()='Copy Text']");
-    await copytext.scrollIntoViewIfNeeded();
-    await page.waitForTimeout(3000);
-    await copytext.dblclick();
-    let field=await page.locator("//input[@id='field2']");
-    await expect(field).toHaveValue("Hello World!");
-    await page.waitForTimeout(3000);
+    // await page.goto("https://testautomationpractice.blogspot.com/");
+    // await page.waitForTimeout(4000);
+    // let copytext = await page.locator("//button[text()='Copy Text']");
+    // await copytext.scrollIntoViewIfNeeded();
+    // await page.waitForTimeout(3000);
+    // await copytext.dblclick();
+    // let field=await page.locator("//input[@id='field2']");
+    // await expect(field).toHaveValue("Hello World!");
+    // await page.waitForTimeout(3000);
 
 })

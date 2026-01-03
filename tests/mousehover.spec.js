176843b7a53a1c5a895a4ee022e8b47538b41e7b
vcpatil1990 <vcp199013@gmail.com>
2026-01-03 21:14:37 +0530

    Update mousehover.spec.js

--- tests/mousehover.spec.js
+++ tests/mousehover.spec.js
@@ -2,8 +2,8 @@ const { test, expect } = require('@playwright/test');
 
 test('Mouse hover ', async ({ page }) => {
 
-    await page.goto("https://www.myntra.com/mantra");
-    await page.waitForTimeout(3000);
+    // await page.goto("https://www.myntra.com/mantra");
+    // await page.waitForTimeout(3000);
 
     let kids = await page.locator("//div[@class='desktop-navLink']//a[text()='Kids']");
     await kids.hover();
